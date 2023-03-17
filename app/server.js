6be3f09f041bd0df8a078aa8519ade7291fc81b9
alexandru.patroi <alexandru.patroi@opswat.com>
2023-03-17 17:14:42 +0200

    add volume to the docker container

--- app/server.js
+++ app/server.js
@@ -17,11 +17,11 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-// app.get('/profile-picture', function (req, res) {
-//   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-//   res.writeHead(200, {'Content-Type': 'image/jpg' });
-//   res.end(img, 'binary');
-// });
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
 
 // Pay attention to the url
 
@@ -43,7 +43,7 @@ let databaseName = "my-db";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlDockerCompose, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -65,7 +65,7 @@ app.post('/update-profile', function (req, res) {
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlDockerCompose, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
